aa86e9c1ecce082e024408efc5c56c21764d26b8
Matt Rosanio and Michael Smith <michaelanthonysmith00@gmail.com>
2016-01-19 14:12:34 -0800

    take care of any errors when someone does not enter a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 function triangle(side1, side2, side3) {
-  if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)) {
+   if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+     return 'numbers only';
+   } else if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)) {
     return 'NaT';
   } else if ((side1 + side2 + side3)/3 === side1) {
-    return 'equilateral';
+    return 'equilateral triangle';
   } else if ((side1 === side2 && side2 !== side3) ||
 (side2 === side3 && side1 !== side3) || (side1 === side3 && side2 !== side3)){
-    return 'isosceles';
+    return 'isosceles triangle';
   } else {
-    return 'scalene';
+    return 'scalene triangle';
   }
 }
 
@@ -18,8 +20,15 @@ $(function() {
     var side3 = parseInt($('input#side3').val());
     var trian = triangle(side1, side2, side3);
 
-    $('#result').show();
-    $('#triangle').text(trian);
+    if (trian === "numbers only") {
+      $('#threeNumbers').hide();
+      $('#notThreeNumbers').show();
+    } else {
+      $('#notThreeNumbers').hide();
+      $('#threeNumbers').show();
+      $('#triangle').text(trian);
+    }
+
     event.preventDefault();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,21 @@
 describe('triangle', function() {
   it("is true for a triangle which has 3 equal sides", function() {
-    expect(triangle(3, 3, 3)).to.equal('equilateral');
+    expect(triangle(3, 3, 3)).to.equal('equilateral triangle');
   });
 
   it("is true for a triangle which has 2 equal sides", function() {
-    expect(triangle(3, 3, 2)).to.equal('isosceles');
+    expect(triangle(3, 3, 2)).to.equal('isosceles triangle');
   });
 
   it("is true for a triangle which has no equal sides", function() {
-    expect(triangle(4, 3, 2)).to.equal('scalene');
+    expect(triangle(4, 3, 2)).to.equal('scalene triangle');
   });
 
   it("is true for a triangle which the sum of the lengths of any two sides is less than or equal to the length of the third side", function() {
     expect(triangle(3, 2, 1)).to.equal('NaT');
+  });
+
+  it("will bring up an error message if user enters a letter", function() {
+    expect(triangle(2, 2, 'l')).to.equal('numbers only');
   });
 });
