d6b54d0272fa87048be8338c825ee77148f4fcc5
Matt Rosanio and Michael Smith <mmrosani@syr.edu>
2016-01-19 09:42:58 -0800

    add user logic with jquery, and content to homepage. User can now enter sides and figure out what kind of triangle they are.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,16 @@ function triangle(side1, side2, side3) {
     return 'scalene';
   }
 }
+
+$(function() {
+  $('form#sides').submit(function(event) {
+    var side1 = parseInt($('input#side1').val());
+    var side2 = parseInt($('input#side2').val());
+    var side3 = parseInt($('input#side3').val());
+    var trian = triangle(side1, side2, side3);
+
+    $('#result').show();
+    $('#triangle').text(trian);
+    event.preventDefault();
+  });
+});
