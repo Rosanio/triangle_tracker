8c5a7fe4cb2a294e124b84d0e161a63ade9a614c
Matt Rosanio and Michael Smith <mmrosani@syr.edu>
2016-01-19 09:25:52 -0800

    add logic to pass not triangle test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function triangle(side1, side2, side3) {
-  if (side1 === side2 && side2 === side3) {
+  if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)) {
+    return 'NaT';
+  } else if ((side1 + side2 + side3)/3 === side1) {
     return 'equilateral';
   } else if ((side1 === side2 && side2 !== side3) ||
 (side2 === side3 && side1 !== side3) || (side1 === side3 && side2 !== side3)){
@@ -7,5 +9,4 @@ function triangle(side1, side2, side3) {
   } else {
     return 'scalene';
   }
-
 }
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,8 @@ describe('triangle', function() {
   it("is true for a triangle which has no equal sides", function() {
     expect(triangle(4, 3, 2)).to.equal('scalene');
   });
+
+  it("is true for a triangle which the sum of the lengths of any two sides is less than or equal to the length of the third side", function() {
+    expect(triangle(3, 2, 1)).to.equal('NaT');
+  });
 });
